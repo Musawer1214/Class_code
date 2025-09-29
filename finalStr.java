04ef0deef06cea1df17c6c423da652ef071d5bc8
Musawer1214 <musawerhussain1214@gmail.com>
2025-09-29 22:49:02 +0800

    new code update

---
+++ finalStr.java
@@ -0,0 +1,13 @@
+public class finalStr {
+    public static void main(String[] args) {
+        private final StringBuilder strb;
+
+        strb = new StringBuilder();
+
+
+        strb.append("Hello");
+        strb.append(" World!");
+
+        System.out.println(strb);
+    }
+}
